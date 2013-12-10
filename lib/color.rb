bed3f7ff7eaf7909c3544534bf049ea6ab3746c6
sranso <sranso@gmail.com>
2013-12-09 19:18:19 -0500

    add files

--- lib/color.rb
+++ lib/color.rb
@@ -1,7 +1,7 @@
 class Color
 
   def rando
-    ["AFEEEE", "66CDAA", "20B2AA", "008080", "6A5ACD", "48D1CC", "98FB98"].sample
+    ["AFEEEE", "66CDAA", "20B2AA", "008080", "6A5ACD", "48D1CC", "D398FB"].sample
   end
 
 end
