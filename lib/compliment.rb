5cc1753f793f0692b31cb744adcd50fd0bd501f6
sranso <sranso@gmail.com>
2013-12-12 14:19:50 -0500

    update compliment msgs

--- lib/compliment.rb
+++ lib/compliment.rb
@@ -1,22 +1,11 @@
 class Compliment
 
-  MESSAGES = [
-    "You know more today than you did yesterday.",
-    "Your code gets better every day.",
-    "You'll get there, keep trying.",
-    "You got in! Therefore you deserve to be here.",
-    "Stop comparing yourself--you're great.",
-    "Take a breath, take a break.",
-    "There is more to life than code. But for now, learn what you can.",
-    "You're getting back as much as your putting in. Remember that.",
-    "Look around you! Aren't these people great?",
-    "\"If you keep putting bricks on top of each other, it might take a long time but eventually you’ll have a wall.\"",
-    "Remember when you didn't know what a hash was? You've come so far!",
-    "Enjoy these moments."
-  ]
-
   def message
-    MESSAGES.sample
+    @messages = []
+    File.readlines('./public/compliments.txt').each do |line|
+      @messages << line
+    end
+    @messages.sample
   end
 
 end
