3992e0fb2acc28be8fe36031f4baea46c1a9b78c
sranso <sranso@gmail.com>
2013-12-12 19:26:53 -0500

    no repeats of color or msg back-to-back

--- app.rb
+++ app.rb
@@ -5,10 +5,33 @@ Bundler.require
 
 class EmergencyCompliments < Sinatra::Application
 
+  before do
+    @@prev_msg ||= nil
+    @@prev_color ||= nil
+  end
+
   get '/' do
-    @compliment = Compliment.new
-    @color = Color.new
+    @compliment = Compliment.new.message
+    same_as_last_msg?
+    @@prev_msg = @compliment
+
+    @color = Color.new.rando
+    same_as_last_color?
+    @@prev_color = @color
     erb :compliment
   end
 
+  helpers do
+    def same_as_last_msg?
+      while @compliment == @@prev_msg
+        @compliment = Compliment.new.message
+      end
+    end
+
+    def same_as_last_color?
+      while @color == @@prev_color
+        @color = Color.new.rando
+      end
+    end
+  end
 end
